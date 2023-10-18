e8c46e3c16923b26d840b9e5b9ead602ea20329d
0xRock <cihankaya371@gmail.com>
2023-10-18 23:07:00 +0200

    &lsquo;

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
                 {/* <img style={{borderRadius: 10}} src={profilePicture} alt="Follow me =)" height={96} width={96} /> */}
                 {/* <Image priority src={starIcon} height={150} width={150} className="starknet" alt="Follow me on Twitter =) 0xRockW3" /> */}
                 <h1 className='textLogo'>
-                    <span>0xRock's</span>
+                    <span>0xRock&lsquo;s</span>
                     <span>Ratings DApp</span>
                 </h1>
             </div>
